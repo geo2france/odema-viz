0cbcd7cb7d8a6c44b3e85239c22745698c0dceb0
nlemahieu-open <nicolas.lemahieu@open-groupe.com>
2023-07-25 10:17:04 +0200

    handle errors for production

--- src/views/TechnicalSheet.tsx
+++ src/views/TechnicalSheet.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Header } from '../components/Header/Header';
 
 export default () => {
-  const { id } = useParams<{ id: string }>();
-
   useEffect(() => {}, []);
 
   return (
