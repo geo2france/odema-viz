65f5f4cea6d0fafcb03f02ebbd4bbf21bcdf4a76
nlemahieu-open <nicolas.lemahieu@open-groupe.com>
2023-08-11 11:15:37 +0200

    [DREALODEMA-148] display sum and average per year

--- src/components/TableTabulator/TableTabulator.tsx
+++ src/components/TableTabulator/TableTabulator.tsx
@@ -52,6 +52,57 @@ export default ({ minMaxYearRange, filteredData, unitSelected }: Props) => {
     }
   };
 
+  const createSumOfTerritoriesValuesPerYear = (summedByTerritory: any) => {
+    let sumByYear: any = {};
+
+    Object.keys(summedByTerritory).forEach((territoryName: string) => {
+      Object.keys(summedByTerritory[territoryName]).forEach(
+        (yearValue: string) => {
+          if (!sumByYear.hasOwnProperty(yearValue)) {
+            sumByYear = { ...sumByYear, [yearValue]: 0 };
+          }
+
+          sumByYear[yearValue] =
+            sumByYear[yearValue] +
+            (summedByTerritory[territoryName][yearValue] ?? 0);
+        }
+      );
+    });
+
+    return sumByYear;
+  };
+
+  const createAverageOfTerritoriesValuesPerYear = (
+    summedByTerritory: any,
+    sumByYear: any
+  ) => {
+    let coefficientsPeryear: any = {};
+    let finalAverage: any = {};
+
+    Object.keys(summedByTerritory).map((territoryName: string) => {
+      Object.keys(summedByTerritory[territoryName]).map((year: string) => {
+        if (!coefficientsPeryear.hasOwnProperty(year)) {
+          coefficientsPeryear = { ...coefficientsPeryear, [year]: 0 };
+        }
+
+        if (summedByTerritory[territoryName].hasOwnProperty(year)) {
+          coefficientsPeryear[year] = coefficientsPeryear[year] + 1;
+        }
+      });
+    });
+
+    Object.keys(coefficientsPeryear).map((yearCoefficient: string) => {
+      if (!finalAverage.hasOwnProperty(yearCoefficient)) {
+        finalAverage = { ...finalAverage, [yearCoefficient]: 0 };
+      }
+
+      finalAverage[yearCoefficient] =
+        sumByYear[yearCoefficient] / coefficientsPeryear[yearCoefficient];
+    });
+
+    return finalAverage;
+  };
+
   const roundValues = (summedByTerritory: any) => {
     Object.keys(summedByTerritory).map((territoryName: string) => {
       for (let year in summedByTerritory[territoryName]) {
@@ -91,20 +142,20 @@ export default ({ minMaxYearRange, filteredData, unitSelected }: Props) => {
       );
     });
 
-    let sumByYear: any = {};
-    Object.keys(summedByTerritory).forEach((territoryName: string) => {
-      Object.keys(summedByTerritory[territoryName]).forEach(
-        (yearValue: string) => {
-          if (!sumByYear.hasOwnProperty(yearValue)) {
-            sumByYear = { ...sumByYear, [yearValue]: 0 };
-          }
+    const sumByYear = {
+      territory: 'Somme totale par année',
+      ...createSumOfTerritoriesValuesPerYear(summedByTerritory),
+    };
+    const averageByYear = {
+      territory: 'Moyenne par année',
+      ...createAverageOfTerritoriesValuesPerYear(summedByTerritory, sumByYear),
+    };
 
-          sumByYear[yearValue] =
-            sumByYear[yearValue] +
-            (summedByTerritory[territoryName][yearValue] ?? 0);
-        }
-      );
-    });
+    summedByTerritory = {
+      ...summedByTerritory,
+      sumByYear,
+      averageByYear,
+    };
 
     //We round values to two decimal
     roundValues(summedByTerritory);
