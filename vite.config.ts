c1b67d3d392ef1d56bec421a6a725fb4396a774a
nlemahieu-open <nicolas.lemahieu@open-groupe.com>
2023-08-11 12:47:46 +0200

    [DREALODEMA-147] vite config binded to basename

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/public/odema/indicateur_dev',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 });
