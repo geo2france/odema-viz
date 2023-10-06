c236806ffcaa6d0d996f67b62fb0187644db83ce
Maxime Guillemin <maxime.guillemin@hautsdefrance.fr>
2023-10-06 10:13:35 +0200

    couleur de l'odema

--- src/theme/lightThemeConfig.tsx
+++ src/theme/lightThemeConfig.tsx
@@ -3,5 +3,8 @@ import { createTheme } from "@mui/material/styles";
 export const lightThemeConfig = createTheme({
   palette: {
     mode: "light",
+    primary: {
+      main: "#FF5F4A",
+    },
   },
 });
